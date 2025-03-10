6f1f994e48b7188add324fa7ea696aa456916ede
malaheaven <yunhg95@gmail.com>
2025-03-10 14:17:43 +0900

    feat:spring init

---
+++ src/main/java/com/codeDuo/myTime/MyTimeApplication.java
@@ -0,0 +1,13 @@
+package com.codeDuo.myTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeDuo/myTime/MyTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeDuo.myTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
